f03905d9f0798a1be5856d49b912e847fe0b1332
unknown <dudtjr913@naver.com>
2020-12-07 12:15:49 +0900

    Update : splitedInput move to onSubmitUserInput
    <description>
    다음 기능 구현 사항인 단어를 밀어내는 함수에서도 splitedInput이 필요하기
    때문에 인자로 넘겨줄 수 있는 onSubmitUserInput으로 자리를 이동했고,
    input 검사와 단어를 밀어내는 함수의 파라미터로 넘겨줄 것이다.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ const $cubeForm = document.body.querySelector('form');
 const $cubeInput = $cubeForm.querySelector('input');
 
 const onSubmitUserInput = () => {
-  if (!isUserInputValid()) {
+  const splitedInput = $cubeInput.value.split(' ');
+  if (!isUserInputValid(splitedInput)) {
     return ($cubeInput.value = '');
   }
 };
 
-const isUserInputValid = () => {
-  const splitedInput = $cubeInput.value.split(' ');
-  if (splitedInput.length !== 3) {
+const isUserInputValid = (userInput) => {
+  if (userInput.length !== 3) {
     return alert('띄어쓰기로 구분하여 단어 정수 L/R 형식으로 입력해주세요.');
   }
-  if (splitedInput[0].match(/\W/)) {
+  if (userInput[0].match(/\W/)) {
     return alert('영어 단어를 입력해주세요.');
   }
-  if (!(splitedInput[1] >= -100 && splitedInput[1] < 100)) {
+  if (!(userInput[1] >= -100 && userInput[1] < 100)) {
     return alert('-100 ~ 99까지의 정수를 입력해주세요.');
   }
-  if (splitedInput[2].match(/[^lr]/i)) {
+  if (userInput[2].match(/[^lr]/i)) {
     return alert('L(l) 또는 R(r)을 입력해주세요.');
   }
 
