751f543a58ddf1f14a5929acf2c1f4cdeff87383
unknown <dudtjr913@naver.com>
2020-12-07 15:28:59 +0900

    Refactor : Complete push word function modularization
    <description>
    단어를 미는 함수인 pushUserSelectedWord 함수를 push-word.js파일로 옮겨
    모듈화 하였다.
    이렇게 모듈화를 모두 마쳤고, 모듈화의 기준은 다른 함수에서도 사용될 수
    있을 만한 것을 모듈화했다.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import isUserInputValid from './input-valid.js';
+import pushUserSelectedWord from './push-word.js';
 
 const startCubeGame = (function () {
   const $cubeForm = document.body.querySelector('form');
@@ -13,41 +14,6 @@ const startCubeGame = (function () {
     printResultOnScreen($cubeInput.value, pushedWordResult);
 
     return ($cubeInput.value = '');
-  };
-
-  const pushUserSelectedWord = (splitedInput) => {
-    const [word, count, direction] = splitedInput;
-    const upperCaseDirection = direction.toUpperCase();
-    const remainsCount = count % word.length; // 나머지 숫자
-    if (
-      (count >= 0 && upperCaseDirection === 'L') ||
-      (count < 0 && upperCaseDirection === 'R')
-    ) {
-      return pushWordLeft(word, Math.abs(remainsCount));
-    }
-
-    if (
-      (count >= 0 && upperCaseDirection === 'R') ||
-      (count < 0 && upperCaseDirection === 'L')
-    ) {
-      return pushWordRight(word, Math.abs(remainsCount));
-    }
-  };
-
-  const pushWordLeft = (word, count) => {
-    const splitedWord = word.split('');
-    const pushedWord = splitedWord.splice(0, count);
-    const result = [...splitedWord, ...pushedWord];
-
-    return result.join('');
-  };
-
-  const pushWordRight = (word, count) => {
-    const splitedWord = word.split('');
-    const pushedWord = splitedWord.splice(word.length - count);
-    const result = [...pushedWord, ...splitedWord];
-
-    return result.join('');
   };
 
   const printResultOnScreen = (userInput, resultWord) => {
