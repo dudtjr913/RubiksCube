b01d2b9903ed990d370368aae3f93adef5547ba3
unknown <dudtjr913@naver.com>
2020-12-07 16:36:34 +0900

    Feat : 유저의 input을 받는 event 추가
    <description>
    input을 받는 form에 submit 이벤트를 등록했다.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import Cube from './class/cube.js';
+
+const $cubeForm = document.body.querySelector('form');
+const $cubeInput = $cubeForm.querySelector('input');
+
+const onSubmitUserInput = () => {};
+
+$cubeForm.addEventListener('submit', onSubmitUserInput);
