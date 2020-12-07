f5d0bc09bd915861aafc4ad46b398de5be4d5865
unknown <dudtjr913@naver.com>
2020-12-07 22:08:12 +0900

    Refactor : IIFE로 은닉화
    <description>
    IIFE로 감싸주어 은닉화를 했다.

--- src/index.js
+++ src/index.js
@@ -7,50 +7,52 @@ import {
   removeResultScreen,
 } from './screen.js';
 
-const cubeGame = new Cube();
-const $cubeForm = document.body.querySelector('form');
-const $cubeInput = $cubeForm.querySelector('input');
+(function () {
+  const cubeGame = new Cube();
+  const $cubeForm = document.body.querySelector('form');
+  const $cubeInput = $cubeForm.querySelector('input');
 
-const onSubmitUserInput = () => {
-  const matchedUserInput = matchUserInput($cubeInput.value);
-  if ($cubeInput.value === QUIT_INPUT) {
-    return finishGame();
-  }
-  if (isInputValid(matchedUserInput, $cubeInput.value)) {
-    matchedUserInput.forEach((side) => {
-      moveCube(side);
-      createResultScreen(cubeGame, side);
-    });
-  }
+  const onSubmitUserInput = () => {
+    const matchedUserInput = matchUserInput($cubeInput.value);
+    if ($cubeInput.value === QUIT_INPUT) {
+      return finishGame();
+    }
+    if (isInputValid(matchedUserInput, $cubeInput.value)) {
+      matchedUserInput.forEach((side) => {
+        moveCube(side);
+        createResultScreen(cubeGame, side);
+      });
+    }
 
-  return ($cubeInput.value = '');
-};
+    return ($cubeInput.value = '');
+  };
 
-const matchUserInput = (userInput) => {
-  const matchedUserInput = userInput.match(/U'|U|R'|R|L'|L|B'|B/g);
+  const matchUserInput = (userInput) => {
+    const matchedUserInput = userInput.match(/U'|U|R'|R|L'|L|B'|B/g);
 
-  return matchedUserInput;
-};
+    return matchedUserInput;
+  };
 
-const moveCube = (side) => {
-  if (side === cubeSide.UP_LEFT || side === cubeSide.UP_RIGHT) {
-    return cubeGame.moveUp(side);
-  }
-  if (side === cubeSide.DOWN_LEFT || side === cubeSide.DOWN_RIGHT) {
-    return cubeGame.moveDown(side);
-  }
-  if (side === cubeSide.LEFT_UP || side === cubeSide.LEFT_DOWN) {
-    return cubeGame.moveLeft(side);
-  }
-  if (side === cubeSide.RIGHT_UP || side === cubeSide.RIGHT_DOWN) {
-    return cubeGame.moveRight(side);
-  }
-};
+  const moveCube = (side) => {
+    if (side === cubeSide.UP_LEFT || side === cubeSide.UP_RIGHT) {
+      return cubeGame.moveUp(side);
+    }
+    if (side === cubeSide.DOWN_LEFT || side === cubeSide.DOWN_RIGHT) {
+      return cubeGame.moveDown(side);
+    }
+    if (side === cubeSide.LEFT_UP || side === cubeSide.LEFT_DOWN) {
+      return cubeGame.moveLeft(side);
+    }
+    if (side === cubeSide.RIGHT_UP || side === cubeSide.RIGHT_DOWN) {
+      return cubeGame.moveRight(side);
+    }
+  };
 
-const finishGame = () => {
-  $cubeForm.removeEventListener('submit', onSubmitUserInput);
-  removeResultScreen();
-  createFinishScreen();
-};
+  const finishGame = () => {
+    $cubeForm.removeEventListener('submit', onSubmitUserInput);
+    removeResultScreen();
+    createFinishScreen();
+  };
 
-$cubeForm.addEventListener('submit', onSubmitUserInput);
+  $cubeForm.addEventListener('submit', onSubmitUserInput);
+})();
