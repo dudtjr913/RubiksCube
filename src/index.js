1526e99918c10d103b5378014e52d5f1ae3651b6
unknown <dudtjr913@naver.com>
2020-12-07 13:45:22 +0900

    Feat : create Right push function
    <description>
    오른쪽으로 단어를 밀어내는 함수를 구현했다.
    왼쪽으로 밀어낼 때와 마찬가지로 splitedWord와 pushedWord가 있고,
    왼쪽으로 밀어낼 때와는 반대로 result값에서 pushedWord가 먼저 나오고
    splitedWord가 뒤에 나오도록 했다.

--- src/index.js
+++ src/index.js
@@ -57,6 +57,12 @@ const pushWordLeft = (word, count) => {
   return result.join('');
 };
 
-const pushWordRight = (word, count) => {};
+const pushWordRight = (word, count) => {
+  const splitedWord = word.split('');
+  const pushedWord = splitedWord.splice(word.length - count);
+  const result = [...pushedWord, ...splitedWord];
+
+  return result.join('');
+};
 
 $cubeForm.addEventListener('submit', onSubmitUserInput);
