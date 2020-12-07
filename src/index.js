168f55fb5527d143236c6dfe64244e802948c894
unknown <dudtjr913@naver.com>
2020-12-07 11:50:41 +0900

    Feat : Add form-submit event <description> 사용자의 입력을 받을 수 있도록 addEventListener을 추가하였다. 이제부터 사용자가 input을 submit할 때마다 onSubmitUserInput함수가 실행된다.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const $cubeForm = document.body.querySelector('form');
+const $cubeInput = $cubeForm.querySelector('input');
+
+const onSubmitUserInput = () => {};
+
+$cubeForm.addEventListener('submit', onSubmitUserInput);
