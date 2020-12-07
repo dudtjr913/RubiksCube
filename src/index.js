02c9d8b386947a5bb12381f1b1b23eb0aea42d63
unknown <dudtjr913@naver.com>
2020-12-07 16:31:39 +0900

    Feat : Cube 클래스 생성 및 프로퍼티, 메소드 구현
    <description>
    초기의 큐브를 이중 배열로 할당하였고, Up, Down, Left, Right로 움직이기
    위한 메소드를 추가하였다.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Cube {
+  constructor() {
+    this.cube = [
+      [R, R, W],
+      [G, C, W],
+      [G, B, B],
+    ];
+  }
+
+  moveUp() {}
+
+  moveDown() {}
+
+  moveLeft() {}
+
+  moveRight() {}
+}
