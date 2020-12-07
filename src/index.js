ffc2ff247c10636ec31599f089117f941602c489
unknown <dudtjr913@naver.com>
2020-12-07 12:11:27 +0900

    Feat : Check input valid
    <description>
    사용자의 입력의 유효성을 체크하는 함수 isUserInputValid 구현
    1. 띄어쓰기로 구분하지 않거나 필수 사항을 누락한 경우 - 에러
    2. 영어 단어를 입력하지 않은 경우
    3. 정수의 범위를 벗어난 경우
    4. L 또는 R을 입력하지 않은 경우

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@
 const $cubeForm = document.body.querySelector('form');
 const $cubeInput = $cubeForm.querySelector('input');
 
-const onSubmitUserInput = () => {};
+const onSubmitUserInput = () => {
+  if (!isUserInputValid()) {
+    return ($cubeInput.value = '');
+  }
+};
+
+const isUserInputValid = () => {
+  const splitedInput = $cubeInput.value.split(' ');
+  if (splitedInput.length !== 3) {
+    return alert('띄어쓰기로 구분하여 단어 정수 L/R 형식으로 입력해주세요.');
+  }
+  if (splitedInput[0].match(/\W/)) {
+    return alert('영어 단어를 입력해주세요.');
+  }
+  if (!(splitedInput[1] >= -100 && splitedInput[1] < 100)) {
+    return alert('-100 ~ 99까지의 정수를 입력해주세요.');
+  }
+  if (splitedInput[2].match(/[^lr]/i)) {
+    return alert('L(l) 또는 R(r)을 입력해주세요.');
+  }
+
+  return true;
+};
 
 $cubeForm.addEventListener('submit', onSubmitUserInput);
