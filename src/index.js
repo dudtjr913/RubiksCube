6ae1b47fbb755ed8979b17277537f5b09c78b693
unknown <dudtjr913@naver.com>
2020-12-07 17:14:17 +0900

    Feat : 입력이 올바를 때 Cube 클래스의 메소드 호출
    <description>
    입력이 올바를 때 matched된 input을 돌면서 맞는 포지션의 메소드를
    호출한다.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,30 @@ const onSubmitUserInput = () => {
   if (!isInputValid(matchedUserInput, $cubeInput.value)) {
     return ($cubeInput.value = '');
   }
+  matchedUserInput.forEach((position) => {
+    moveCube(position);
+  });
 };
 
 const matchUserInput = (userInput) => {
   const matchedUserInput = userInput.match(/U'|U|R'|R|L'|L|B'|B/g);
 
   return matchedUserInput;
+};
+
+const moveCube = (position) => {
+  if (position === 'U' || position === "U'") {
+    return cubeGame.moveUp(position);
+  }
+  if (position === 'B' || position === "B'") {
+    return cubeGame.moveDown(position);
+  }
+  if (position === 'L' || position === "L'") {
+    return cubeGame.moveLeft(position);
+  }
+  if (position === 'R' || position === "R'") {
+    return cubeGame.moveRight(position);
+  }
 };
 
 $cubeForm.addEventListener('submit', onSubmitUserInput);
