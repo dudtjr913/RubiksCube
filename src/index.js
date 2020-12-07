9149836848c9efea3ad27b1f1f664f81dbc43010
unknown <dudtjr913@naver.com>
2020-12-07 13:36:34 +0900

    Update : Change check input valid function's logic
    <description>
    정수 입력 란에 빈 칸을 입력했을 때 자동으로 0이 부여되는 것과 \W는 영어
    단어가 아닌 영어 단어와 숫자를 제외한 정규표현식임을 깨닫고 영어
    단어만을 받기 위해 [^a-zA-Z]를 사용했다. 이때 빈 칸을 입력하면 문제 없이
    출력되기 때문에 빈 칸을 입력하지 못하도록 some을 사용했다.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ const isUserInputValid = (userInput) => {
   if (userInput.length !== 3) {
     return alert('띄어쓰기로 구분하여 단어 정수 L/R 형식으로 입력해주세요.');
   }
-  if (userInput[0].match(/\W/)) {
+  if (userInput.some((input) => input === '')) {
+    return alert('빈 칸은 입력할 수 없습니다.');
+  }
+  if (userInput[0].match(/[^a-zA-Z]/)) {
     return alert('영어 단어를 입력해주세요.');
   }
   if (!(userInput[1] >= -100 && userInput[1] < 100)) {
