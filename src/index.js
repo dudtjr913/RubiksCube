19748d9eba7d4938b71a3b555dc287a8092611a1
unknown <dudtjr913@naver.com>
2020-12-08 22:33:15 +0900

    Feat : 필요한 값만 받는 함수 구현 완료
    <description>
    필요한 값인 U'|D'|F'|B'|L'|R'|U|D|F|B|L|R|2 를 정규표현식을 사용하여
    return했다. 아직은 하드코딩인 것 같지만 나중에 추가로 최대한 리팩토링을 할 수
    있도록 해볼 것이다.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ const $userForm = document.body.querySelector('form');
 const $userInput = $userForm.querySelector('input');
 
 const onSubmitUserInput = () => {
-  console.log($userInput.value);
+  const matchedInput = extractMatchedInput();
+};
+
+const extractMatchedInput = () => {
+  const matchedInput = $userInput.value.match(
+    /U'|D'|F'|B'|L'|R'|U|D|F|B|L|R|2/g,
+  );
+
+  return matchedInput;
 };
 
 $userForm.addEventListener('submit', onSubmitUserInput);
