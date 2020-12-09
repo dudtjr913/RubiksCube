2f52e968f02c51ce0768fb0304c8867523337714
unknown <dudtjr913@naver.com>
2020-12-09 17:57:48 +0900

    Refactor : 게임 종료 시 text 상수로 변경
    <description>
    조작갯수와 마무리 멘트를 utils.js에서 상수화 시켜서 관리한다.

--- src/screen.js
+++ src/screen.js
@@ -1,3 +1,5 @@
+import {FINISH_MESSAGE} from './utils.js';
+
 export const createResultScreen = (cube, formula) => {
   const $screenUl = document.body.querySelector('#cube-list');
   const $screenLi = document.createElement('li');
@@ -30,8 +32,8 @@ export const createFinishTextOnScreen = (count) => {
   const $cubeContainer = document.body.querySelector('#cube');
   const $finishContainer = document.createElement('div');
   $finishContainer.innerHTML = `
-  <div>조작갯수 : ${count}</div>
-  <div>이용해주셔서 감사합니다. 뚜뚜뚜.</div>
+  <div>${FINISH_MESSAGE.TURN_COUNT} ${count}</div>
+  <div>${FINISH_MESSAGE.THANK_MESSAGE}</div>
   `;
   $cubeContainer.appendChild($finishContainer);
 };
--- src/utils.js
+++ src/utils.js
@@ -18,3 +18,8 @@ export const ERROR_MESSAGE = {
   WRONG_INPUT: "2, U('), D('), F('), B('), R('), L(')만 입력할 수 있습니다.",
   LENGTH_LIMIT: '20개 이하로 입력해주세요.',
 };
+
+export const FINISH_MESSAGE = {
+  TURN_COUNT: '조작갯수 : ',
+  THANK_MESSAGE: '이용해주셔서 감사합니다. 뚜뚜뚜.',
+};
