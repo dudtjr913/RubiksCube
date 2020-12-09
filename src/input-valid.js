3539d050b1aca724009ccdb85cc3222941c05b6e
unknown <dudtjr913@naver.com>
2020-12-09 17:11:07 +0900

    Update : 사용자의 input을 검사할 때의 로직 수정
    <description>
    input을 검사할 때 정규표현식을 사용해 필요한 value만 가져오는
    matchedInput을 매개변수로 받았었는데, 하나도 match가 되지 않아 null이
    출력되었을 때 따로 예외처리를 하지 않아서 기존의 조건문에
    !matchedInput을 추가하였다.

--- src/input-valid.js
+++ src/input-valid.js
@@ -1,5 +1,5 @@
 export default function isInputValid(matchedInput, userInput) {
-  if (userInput !== matchedInput.join('')) {
+  if (!matchedInput || userInput !== matchedInput.join('')) {
     return alert("2, U('), D('), F('), B('), R('), L(')만 입력할 수 있습니다.");
   }
   if (matchedInput.length > 20) {
