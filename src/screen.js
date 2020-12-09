9b10bbcb8b38caadb50b1798f38ca3ab3d0ab54b
unknown <dudtjr913@naver.com>
2020-12-09 16:54:23 +0900

    Feat : 화면에 큐브 출력하는 함수 구현 중
    <description>
    pre 태그를 사용해서 큐브를 하드코딩했다.
    하드코딩을 최대한 하지 않으려고 했는데, 정말 아무리 생각해도 화면에
    출력하는 이 함수는 어떻게 해야할지 모르겠다.. 더 생각해보고 리팩토링을
    할 수 있도록 해야겠다.
    또한, 화면에 입력한 공식을 표시해줘야 하기 때문에 매개변수로 공식도
    받도록 할 것이다. 그리고 지금 현재는 각 큐브마다 ,이 들어가 있기
    때문에 join(' ')을 해줘야하는데, 애초에 넘겨받을 때부터 join을 해서
    넘겨받을 함수를 구현할 것이다.

--- src/screen.js
+++ src/screen.js
@@ -1,1 +1,20 @@
-export const createResultScreen = (cube) => {};
+export const createResultScreen = (cube) => {
+  const $screenUl = document.body.querySelector('#cube-list');
+  const $screenLi = document.createElement('li');
+  $screenLi.innerHTML = `
+    <pre>
+                 ${cube[0][0]}
+                 ${cube[0][1]}
+                 ${cube[0][2]}
+
+  ${cube[1][0]}     ${cube[2][0]}     ${cube[3][0]}     ${cube[4][0]}
+  ${cube[1][1]}     ${cube[2][1]}     ${cube[3][1]}     ${cube[4][1]}
+  ${cube[1][2]}     ${cube[2][2]}     ${cube[3][2]}     ${cube[4][2]}
+
+                 ${cube[5][0]}
+                 ${cube[5][1]}
+                 ${cube[5][2]}
+    </pre>
+  `;
+  $screenUl.appendChild($screenLi);
+};
