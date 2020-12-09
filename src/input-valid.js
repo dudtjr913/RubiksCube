3393e74948b45d32edf2233199e24e1c4ad891fd
unknown <dudtjr913@naver.com>
2020-12-09 17:15:06 +0900

    Refactor : 에러 메시지 - 상수로 변환
    <description>
    에러 메시지는 상수로 빼서 관리하는 것이 더 좋을 것 같아서
    utils.js파일에서 관리하도록 했다.

--- src/input-valid.js
+++ src/input-valid.js
@@ -1,9 +1,11 @@
+import {ERROR_MESSAGE} from './utils.js';
+
 export default function isInputValid(matchedInput, userInput) {
   if (!matchedInput || userInput !== matchedInput.join('')) {
-    return alert("2, U('), D('), F('), B('), R('), L(')만 입력할 수 있습니다.");
+    return alert(ERROR_MESSAGE.WRONG_INPUT);
   }
   if (matchedInput.length > 20) {
-    return alert('20개 이하로 입력해주세요.');
+    return alert(ERROR_MESSAGE.LENGTH_LIMIT);
   }
 
   return true;
--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,8 @@ export const EXTRACT_SIDE = {
   DOWN: 2,
   RIGHT: 3,
 };
+
+export const ERROR_MESSAGE = {
+  WRONG_INPUT: "2, U('), D('), F('), B('), R('), L(')만 입력할 수 있습니다.",
+  LENGTH_LIMIT: '20개 이하로 입력해주세요.',
+};
