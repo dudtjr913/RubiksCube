b68b520109fc4b769c158e138780f977679d2c04
unknown <dudtjr913@naver.com>
2020-12-09 14:10:22 +0900

    Feat : Left회전 시 메소드 구현 완료
    <description>
    Left가 회전할 때 위쪽 큐브의 왼쪽면, 전면 큐브의 왼쪽면, 아래 큐브의
    왼쪽면, 뒷쪽 큐브의 오른쪽면이 움직이고, 이를 표현했다.

--- src/class/cube.js
+++ src/class/cube.js
@@ -60,7 +60,18 @@ export default class Cube {
     changeCube(flatCubes, extractionSides, pushedCube);
   }
 
-  turnLeft(formula) {}
+  turnLeft(formula) {
+    const flatCubes = [this.cube[U], this.cube[F], this.cube[D], this.cube[B]];
+    const extractionSides = [LEFT, LEFT, LEFT, RIGHT];
+    const sideEntry = extractSide(flatCubes, extractionSides);
+    let pushedCube = [];
+    if (formula.includes("'")) {
+      pushedCube = pushCounterClockWise(sideEntry);
+    } else {
+      pushedCube = pushClockWise(sideEntry);
+    }
+    changeCube(flatCubes, extractionSides, pushedCube);
+  }
 
   turnRight(formula) {}
 }
